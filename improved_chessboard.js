ce45054869e3ade2ecccaafd5d6544d5aaa0d7e1
Pierre Celestin Moussa <piercel.coder2022@gmail.com>
2023-07-17 21:56:59 +0200

    Add improved chessboard

---
+++ improved_chessboard.js
@@ -0,0 +1,20 @@
+function generateChessboard(size){
+    if(!Number.isInteger(size) || size <= 0){
+        throw new Error('Size must be a positive integer.');
+    }
+    let board = [];
+
+    for (let y = 0; y < size; y++) {
+        let row = [];
+        for (let x = 0; x < size; x++) {
+            if ((x + y) % 2 === 0) {
+                row.push(" ");
+            } else {
+                row.push("#");
+            }
+        }
+        board.push(row.join(''));
+    }
+    return board.join('\n');
+}
+console.log(chessboard(8))
