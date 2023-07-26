09c54d600f8f32969c21664f942b4a8f7acb6817
Pierre Celestin Moussa <piercel.coder2022@gmail.com>
2023-07-26 16:13:11 +0200

    Add findMedianSortedArrays.js

--- findMedianSortedArrays.js
+++ findMedianSortedArrays.js
@@ -1,19 +1,42 @@
 const findMedianSortedArrays = function (nums1, nums2) {
-    // Merge and sort the arrays
-    const mergedArrays = nums1.concat(nums2);
-    mergedArrays.sort((a, b) => a - b);
+    function mergeSort(arr) {
+        if (arr.length <= 1) {
+            return arr;
+        }
+        const mid = Math.floor(arr.length / 2);
+        const left = arr.slice(0, mid);
+        const right = arr.slice(mid);
+        return merge(mergeSort(left), mergeSort(right));
+    }
 
-    // Obtain the middle (middle-left if there are an even number of elements) point
+    function merge(left, right) {
+        let result = [];
+        let i = 0;
+        let j = 0;
+        while (i < left.length && j < right.length) {
+            if (left[i] < right[j]) {
+                result.push(left[i]);
+                i++;
+            } else {
+                result.push(right[j]);
+                j++;
+            }
+        }
+        return result.concat(left.slice(i)).concat(right.slice(j));
+    }
+
+    function getMedian(arr, middleLeft, isEven = false) {
+        if (isEven) {
+            return (arr[middleLeft] + arr[middleLeft + 1]) / 2;
+        } else {
+            return arr[middleLeft];
+        }
+    }
+
+    const mergedArrays = mergeSort(nums1.concat(nums2));
     const middleLeft = Math.floor(mergedArrays.length / 2);
 
-    // Check if there are an odd number of values
-    if (mergedArrays.length % 2) {
-        // Return the middle value
-        return mergedArrays[middleLeft];
-    } else {
-        // Return the average of the middle 2 values
-        return (mergedArrays[middleLeft] + mergedArrays[middleLeft + 1]) / 2;
-    }
+    return getMedian(mergedArrays, middleLeft, mergedArrays.length % 2 === 0);
 };
-const nums1 = [1,3], nums2 = [2]
+const nums1 = [1, 2, 4], nums2 = [6]
 console.log(findMedianSortedArrays(nums1, nums2));
