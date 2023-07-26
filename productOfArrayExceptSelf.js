95c2e51787d81616f142807e63756edde5469a2c
Pierre Celestin Moussa <piercel.coder2022@gmail.com>
2023-07-26 15:35:56 +0200

    Add the workflow

--- productOfArrayExceptSelf.js
+++ productOfArrayExceptSelf.js
@@ -1,25 +1,29 @@
-function Product(arr){
-        
+function product(arr){
+    if(!Array.isArray(arr)){
+        throw new Error('Input must be an array');
+    }
     const temp = [];
-    
-    
     const product = 1;
     for(let i=0; i<arr.length; i++){
+        if (isNaN(arr[i])) {
+            throw new Error('Input array contains non-numeric values');
+        }
         temp[i] = product;
         product *= arr[i];
     }
-    
     product = 1;
     for(let i=arr.length-1; i>=0; i--){
+        if (isNaN(arr[i])) {
+            throw new Error('Input array contains non-numeric values');
+        }
         temp[i] *= product;
         product *= arr[i];
     }
-    
     return temp;
 }
 
 
 let arr = [2,3,4,5]
 let arr1 = [1,2,3,4]
-console.log(Product(arr));
-console.log(Product(arr1));
+console.log(product(arr));
+console.log(product(arr1));
