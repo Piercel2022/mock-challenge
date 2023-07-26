21c69f6f3dca18c231a047ce343df48d8285c936
Pierre Celestin Moussa <piercel.coder2022@gmail.com>
2023-07-26 15:29:42 +0200

    Add the workflow

--- productOfArrayExceptSelf.js
+++ productOfArrayExceptSelf.js
@@ -1,16 +1,16 @@
 function Product(arr){
         
-    var temp = [];
+    const temp = [];
     
     
-    var product = 1;
-    for(var i=0; i<arr.length; i++){
+    const product = 1;
+    for(let i=0; i<arr.length; i++){
         temp[i] = product;
         product *= arr[i];
     }
     
     product = 1;
-    for(var i=arr.length-1; i>=0; i--){
+    for(let i=arr.length-1; i>=0; i--){
         temp[i] *= product;
         product *= arr[i];
     }
