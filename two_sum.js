8ce39d321dedee0c7514f1fef8d4bb3a85ede323
Pierre Celestin Moussa <piercel.coder2022@gmail.com>
2023-07-26 14:48:10 +0200

    Add two sum in javaScript

---
+++ two_sum.js
@@ -0,0 +1,11 @@
+function twoSum(nums, target) {let numObj = {};
+  for (let i = 0; i < nums.length; i++) {
+    let complement = target - nums[i];
+    if (numObj[complement] !== undefined) {
+      return [numObj[complement], i];
+    }
+    numObj[nums[i]] = i;
+  }
+}
+nums = [2, 7, 11, 15]
+console.log(twoSum(nums, 9))
